efda962712cd0c567944136ed6491d84206e5f85
wolvgvng <jacekobst1@gmail.com>
2018-12-14 10:53:38 +0100

    easy way

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,103 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <h1>THINGS TO DO:</h1>
+    <div class='row form-group mt-5'>
+        <input type='text' @keyup.enter="addItem" v-model='value' class='col-10 col-md-4 mx-auto p-2 border' />
+        <div class='w-100' />
+        <button @click='addItem' class='col-2 col-md-1 mx-auto mt-4 btn btn-dark' type=button>Add
+        </button>
+    </div>
+        <div class='w-100'></div>
+        <br>
+         <ul class='col-11 col-sm-10 col-md-7 col-xl-5 list-unstyled mx-auto' v-for='Item in Items' :key='Item.id'>
+           <li v-if="Item.value!=''">
+             <div class='row py-2'>
+              <button type='button' class='col-2 col-l-1 btn btn-outline-dark btn-sm' @click='deleteItem(Item.id)'>Delete</button>
+              <button type='button' class='col-2 col-l-1 btn btn-outline-dark btn-sm' @click='changeState(Item.id)'>Done</button>
+              <p class='col-7 col-l-9 mt-3 text-center' :class='{done: Item.done}'>{{ Item.value }}</p>
+            </div>
+            <hr>
+           </li>
+         </ul>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+let i = 0;
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      id: '',
+      value: '',
+      Items: [],
+    };
+  },
+  methods: {
+    addItem() {
+      if (this.value !== '') {
+        this.Items[i] = { 
+          id: i,
+          value: this.value,
+          done: false
+          };
+        i += 1;
+        this.value = '';
+      }
+    },
+    deleteItem(id) {
+      this.Items[id] = { id: id, value: '' };
+      let value = this.value;
+      this.value = 'update';
+      this.value = value;
+    },
+    changeState(id) {
+      if (this.Items[id].done === true) this.Items[id].done = false;
+      else this.Items[id].done = true;
+      let value = this.value;
+      this.value = 'update';
+      this.value = value;
+    },
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import '../node_modules/bootstrap/scss/bootstrap.scss';
+
+  h1 {
+    text-align: center;
+    margin: 20px 0;
+  }
+  textarea {
+    height: 30vh;
+  }
+
+  p {
+    overflow: auto;
+  }
+
+  ul {
+    min-height: 0px;
+    margin: 0px;
+  }
+
+  .done {
+    text-decoration: line-through;
+    color: silver;
+  }
+
+  .btn {
+    max-height: 32px;
+    margin-top: 11px;
+
+    @media (max-width: 400px) {
+      font-size: 10px;
+    }
+  }
+
+  button:first-child {
+    margin-right: 4px;
+  }
 </style>
